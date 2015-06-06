1a66c15817f1e27954556019cb7237cbe08ab189
Pedro Abreu <pmig.abreu@gmail.com>
2015-06-06 20:29:30 +0100

    gulp flow fix, removed debug functions from min

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,13 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var ngAnnotate = require('gulp-ng-annotate');
-
-var paths = {
-    templatecache: ['./src/templates/*.html']
-  };
-
+var stripDebug = require('gulp-strip-debug');
+var del = require('del');
 
 gulp.task('default', ['compress','sass'], function() {
-  // place code for your default task here
+  del(['.tmp/'], function (err, paths) {
+    console.log('Deleted files/folders:\n', paths.join('\n'));
+});
 });
 
 gulp.task('lint', function() {
@@ -25,18 +24,21 @@ gulp.task('lint', function() {
 gulp.task('templatecache',['lint'], function () {
     return gulp.src('./src/templates/*.html')
       .pipe(templateCache({standalone:true}))
-      .pipe(gulp.dest('./src/js'));
+      .pipe(gulp.dest('./.tmp/'));
   });
 
 gulp.task('scripts', ['templatecache'], function() {
   return gulp.src('./src/js/*.js')
     .pipe(ngAnnotate())
     .pipe(concat('ion.gallery.js'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./.tmp/'));
 });
 
 gulp.task('compress',['scripts'], function() {
-  return gulp.src('./dist/*.js')
+  return gulp.src('./.tmp/*.js')
+    .pipe(concat('ion.gallery.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(stripDebug())
     .pipe(uglify())
     .pipe(rename('ion.gallery.min.js'))
     .pipe(gulp.dest('./dist'));
