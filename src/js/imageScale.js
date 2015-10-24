10faedbaef9fce7cc9e84e20feac5d4cd59e09cb
Pedro Abreu <pmig.abreu@gmail.com>
2015-10-24 14:48:41 +0100

    fix for issue #8

--- src/js/imageScale.js
+++ src/js/imageScale.js
@@ -15,13 +15,29 @@
     };
 
     function link(scope, element, attrs) {
-      element.bind("load" , function(e){ 
-        
-        if(this.naturalHeight > this.naturalWidth){
+      
+      var scaleImage = function(context,value) {
+        if(value>0){
+          if(context.naturalHeight > context.naturalWidth){
             element.attr('width','100%');
+          }
+          else{
+            element.attr('height',element.parent()[0].offsetHeight+'px');
+          }
+        } 
+      }
+      
+      element.bind("load" , function(e){
+        if(element.parent()[0].offsetHeight > 0){
+          scaleImage(this,element.parent()[0].offsetHeight);
         }
         else{
-          element.attr('height',element.parent()[0].offsetHeight+'px');
+          var _this = this;
+          scope.$watch(function(){
+            return element.parent()[0].offsetHeight;
+          },function(newValue){
+            scaleImage(_this,newValue);
+          })
         }
       });
     }
