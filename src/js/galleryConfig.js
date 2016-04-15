a73838b5bdfe73775682069736af705067845ec0
Daniel Grabert <dg@indivirtuell.net>
2016-04-15 16:00:43 +0200

    configurable template urls

--- src/js/galleryConfig.js
+++ src/js/galleryConfig.js
@@ -10,6 +10,8 @@
   function ionGalleryConfig(){
     this.config = {
       action_label: 'Done',
+      template_gallery: 'gallery.html',
+      template_slider:  'slider.html',
       toggle: true,
       row_size: 3,
       fixed_row_size: true,
